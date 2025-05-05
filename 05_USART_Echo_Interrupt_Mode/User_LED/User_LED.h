da797a1f662c4ef24623d014318ba72bce0de731
zonghan-chen <zonghan_chen@163.com>
2025-05-05 16:50:44 +0800

    Add 05_USART_Echo_Interrupt_Mode

---
+++ 05_USART_Echo_Interrupt_Mode/User_LED/User_LED.h
@@ -0,0 +1,23 @@
+#ifndef USER_LED_H
+#define USER_LED_H
+
+#include "gd32f4xx.h"
+
+#include "systick.h"
+
+typedef enum
+{
+	LED1 = 0,
+	LED2 = 1,
+	LED3 = 2
+} led_typedef_enum;
+
+void led_init(led_typedef_enum lednum);
+void led_on(led_typedef_enum lednum);
+void led_off(led_typedef_enum lednum);
+void led_toggle(led_typedef_enum lednum);
+
+void led123_init(void);
+void led123_flash(uint8_t flash_times);
+
+#endif
